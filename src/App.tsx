3561f9e4d217d1475f0a638ffbcb1a704fba8525
Wen-London <152445401+WenjingLondon@users.noreply.github.com>
2025-10-01 15:47:27 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 
-import React from "react";
-
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 text-white">
