0ac27554b7e00630f33633ccabc97a837f7bcad6
Wenjing Shi <wenjing@Wenjings-MacBook-Air-2.local>
2025-11-10 16:26:01 +0100

    Fix merge conflict and finalize App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 
-<<<<<<< HEAD
-=======
-
->>>>>>> Update App.tsx: update with more code demo
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 text-white">
